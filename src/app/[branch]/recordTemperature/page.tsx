11588f2ba373039e0e105893927cd315a59d1602
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-10-24 14:52:24 +0530

    --

--- src/app/[branch]/recordTemperature/page.tsx
+++ src/app/[branch]/recordTemperature/page.tsx
@@ -1,8 +1,9 @@
 import  RecordTemperatureContainer  from '@/containers/recordTemperatureContainer/RecordTemperatureContainer'
 import React from 'react'
 
-export const page = () => {
+ const page = () => {
   return (
           <RecordTemperatureContainer/>
   )
 }
+export default page;
